3b2474e94c2e51f9813a322426e579c510b53309
Priyam <54617669+PRIYAM266@users.noreply.github.com>
2022-07-20 18:16:01 +0530

    Time: 4 ms (65.57%), Space: 7.9 MB (40.20%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.cpp
+++ 1122-relative-sort-array/1122-relative-sort-array.cpp
@@ -1,25 +1,45 @@
 class Solution {
 public:
     vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
-        vector<int> v;
+        // vector<int> v;
+        // int n = arr1.size();
+        // int m = arr2.size();
+        // int ma = *max_element(arr1.begin(), arr1.end());
+        // int mi = *min_element(arr1.begin(), arr1.end());
+        // vector<int> freq(ma - mi + 1);
+        // for (int i = 0; i < n; i++) {
+        //     freq[arr1[i] - mi]++;
+        // }
+        // for (int i = 0; i < m; i++) {
+        //     while(arr2[i] - mi >= 0 && freq[arr2[i] - mi] > 0) {
+        //         v.push_back(arr2[i]);
+        //         freq[arr2[i] - mi]--;
+        //     }
+        // }
+        // for (int i = 0; i < ma-mi+1; i++) {
+        //     while (freq[i] > 0) {
+        //         v.push_back(i+mi);
+        //         freq[i]--;
+        //     }
+        // }
+        // return v;
         int n = arr1.size();
         int m = arr2.size();
-        int ma = *max_element(arr1.begin(), arr1.end());
-        int mi = *min_element(arr1.begin(), arr1.end());
-        vector<int> freq(ma - mi + 1);
+        vector<int> v;
+        map<int, int> mp;
         for (int i = 0; i < n; i++) {
-            freq[arr1[i] - mi]++;
+            mp[arr1[i]]++;
         }
         for (int i = 0; i < m; i++) {
-            while(arr2[i] - mi >= 0 && freq[arr2[i] - mi] > 0) {
+            while (mp[arr2[i]] > 0) {
                 v.push_back(arr2[i]);
-                freq[arr2[i] - mi]--;
+                mp[arr2[i]]--;
             }
         }
-        for (int i = 0; i < ma-mi+1; i++) {
-            while (freq[i] > 0) {
-                v.push_back(i+mi);
-                freq[i]--;
+        for (auto it = mp.begin(); it != mp.end(); it++) {
+            while (it->second > 0) {
+                v.push_back(it->first);
+                it->second--;
             }
         }
         return v;
