35dac4a4c4e481a127f07cb253a1bf6cd93ecf67
Priyam <54617669+PRIYAM266@users.noreply.github.com>
2022-08-07 00:06:54 +0530

    Time: 187 ms (87.18%), Space: 99.6 MB (21.10%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.cpp
+++ 41-first-missing-positive/41-first-missing-positive.cpp
@@ -5,13 +5,13 @@ public:
         cin.tie(nullptr);
         cout.tie(nullptr);
         int n = nums.size();
-        vector<bool> arr(1000000);
+        vector<bool> arr(600001);
         for (int i = 0; i < n; i++) {
-            if (nums[i] > 0 && nums[i] < 1000000) {
+            if (nums[i] > 0 && nums[i] < 600001) {
                 arr[nums[i]] = true;
             }
         }
-        for (int i = 1; i < 1000000; i++) {
+        for (int i = 1; i < 600001; i++) {
             if (!arr[i])
                 return i;
         }
