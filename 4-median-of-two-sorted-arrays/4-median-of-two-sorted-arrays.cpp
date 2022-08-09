b82f28b493d0a72aba3b157cb3f44e7c62324ddf
Priyam <54617669+PRIYAM266@users.noreply.github.com>
2022-08-10 03:06:01 +0530

    Time: 51 ms (74.92%), Space: 89.2 MB (86.59%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -1,34 +1,34 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        ios_base::sync_with_stdio(0);
-        cin.tie(nullptr);
-        cout.tie(nullptr);
+        // ios_base::sync_with_stdio(0);
+        // cin.tie(nullptr);
+        // cout.tie(nullptr);
         
-        vector<int> arr;
-        int i = 0, j = 0;
+        if (nums1.size() > nums2.size()) {
+            swap(nums1, nums2);
+        }
         int n = nums1.size(), m = nums2.size();
-        while(i < n && j < m) {
-            if (nums1[i] <= nums2[j]) {
-                arr.push_back(nums1[i++]);
-            } else {
-                arr.push_back(nums2[j++]);
-            }
+        int begin1 = 0, end1 = n;
+        while (begin1 <= end1) {
+            int i1 = (begin1+end1)/2;
+            int i2 = (n+m+1)/2 - i1;
+            int max1 = (i1 == 0) ? INT_MIN : nums1[i1 - 1];
+            int max2 = (i2 == 0) ? INT_MIN : nums2[i2 - 1];
+            int min1 = (i1 == n) ? INT_MAX : nums1[i1];
+            int min2 = (i2 == m) ? INT_MAX : nums2[i2];
+            if (max1 <= min2 && max2 <= min1) {
+                if ((n+m) % 2 == 0) {
+                    return ((double) min(min1, min2) + max(max1, max2))/2;
+                } else {
+                    return (double)max(max1, max2);
+                }
+            } 
+            else if (max1 > min2) 
+                end1 = i1-1;
+            else 
+                begin1 = i1+1;
         }
-        while(i < n) {
-            arr.push_back(nums1[i++]);
-        }
-        while(j < m) {
-            arr.push_back(nums2[j++]);
-        }
-        int s = arr.size();
-        float ans;
-        if (s % 2 != 0) {
-            ans = arr[s/2];
-        }
-        else {
-            ans = (float)(arr[s/2]+arr[s/2 - 1]) / 2;
-        }
-        return ans;
+        return 0;
     }
 };
