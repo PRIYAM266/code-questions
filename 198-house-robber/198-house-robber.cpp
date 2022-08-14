2db7e2f5128bc711ecbb8085c0b78379557a10c5
Priyam <54617669+PRIYAM266@users.noreply.github.com>
2022-08-14 23:27:57 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (76.61%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -9,13 +9,15 @@ public:
         if (n == 2) {
             return max(arr[0], arr[1]);
         }
-        vector<int> output(n);
-        output[0] = arr[0];
-        output[1] = max(arr[0], arr[1]);
+        vector<int> dp(n);
+        dp[0] = arr[0];
+        dp[1] = max(arr[0], arr[1]);
         for (int i = 2; i < n; i++) {
-            output[i] = max(output[i-1], output[i-2] + arr[i]);
+            int take = dp[i-2] + arr[i];
+            int not_take = 0 + dp[i-1];
+            dp[i] = max(take, not_take);
         }
     
-        return output[n-1];
+        return dp[n-1];
     }
 };
