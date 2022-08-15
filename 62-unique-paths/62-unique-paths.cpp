0784d193bb95955b89a5d21bd857bea9565ed853
Priyam <54617669+PRIYAM266@users.noreply.github.com>
2022-08-15 17:53:38 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (26.61%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -22,9 +22,9 @@ public:
         // }
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
-                int up = dp[i-1][j];
-                int left = dp[i][j-1];
-                dp[i][j] = up + left;
+                int down = dp[i-1][j];
+                int right = dp[i][j-1];
+                dp[i][j] = down + right;
             }
         }
         return dp[m-1][n-1];
