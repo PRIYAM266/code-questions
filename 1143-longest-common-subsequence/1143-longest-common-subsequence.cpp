d56499887f7786b61ff35187d6626b75ce546589
Priyam <54617669+PRIYAM266@users.noreply.github.com>
2022-08-18 20:31:20 +0530

    Time: 40 ms (44.06%), Space: 13.3 MB (11.47%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,20 +1,20 @@
 class Solution {
     
 public:
-    // int lcs(string &s1, string &s2, int &n1, int &n2, int i, int j, vector<vector<int>> &dp) {
-    //     if (i == n1 || j == n2){
-    //         return 0;
-    //     }
-    //     if (dp[i][j] != -1) {
-    //         return dp[i][j];
-    //     }
-    //     if (s1[i] == s2[j]) {
-    //         return 1 + lcs(s1, s2, n1, n2, i+1, j+1, dp);
-    //     }
-    //     int take1 = lcs(s1, s2, n1, n2, i, j+1, dp);
-    //     int take2 = lcs(s1, s2, n1, n2, i+1, j, dp);
-    //     return dp[i][j] = max(take1, take2);
-    // }
+    int lcs(string &s1, string &s2, int i, int j, vector<vector<int>> &dp) {
+        if (i < 0 || j < 0){
+            return 0;
+        }
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+        if (s1[i] == s2[j]) {
+            return 1 + lcs(s1, s2, i-1, j-1, dp);
+        }
+        int back1 = lcs(s1, s2, i, j-1, dp);
+        int back2 = lcs(s1, s2, i-1, j, dp);
+        return dp[i][j] = max(back1, back2);
+    }
     
     int lcsI(string &s1, string &s2, int &n1, int &n2) {
         
@@ -32,12 +32,10 @@ public:
     }
         
     int longestCommonSubsequence(string text1, string text2) {
-        ios_base::sync_with_stdio(0);
-        cin.tie(nullptr);
-        cout.tie(nullptr);
+        
         int n1 = text1.size(), n2 = text2.size();
-        // vector<vector<int>> dp(n1, vector<int>(n2, -1));
-        // return lcs(text1, text2, n1, n2, 0, 0, dp);
-        return lcsI(text1, text2, n1, n2);
+        vector<vector<int>> dp(n1, vector<int>(n2, -1));
+        return lcs(text1, text2, n1-1, n2-1, dp);
+        // return lcsI(text1, text2, n1, n2);
     }
 };
