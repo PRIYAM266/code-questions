379c5c76618f51352699d602394ee41c6f8f0c8c
Priyam <54617669+PRIYAM266@users.noreply.github.com>
2022-08-25 02:56:13 +0530

    Time: 40 ms (34.75%), Space: 6.1 MB (7.72%) - LeetHub

--- 326-power-of-three/326-power-of-three.cpp
+++ 326-power-of-three/326-power-of-three.cpp
@@ -1,9 +1,6 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        ios_base::sync_with_stdio(0);
-    	cin.tie(nullptr);
-    	cout.tie(nullptr);
         if (n <= 0) return false;
         if (n == 1) return true;
         return fmod((log10(n) / log10(3)), 1) == 0;
