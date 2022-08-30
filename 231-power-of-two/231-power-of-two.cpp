28809ebb58b2634d197aec173cf9b8dd34e3153d
Priyam <54617669+PRIYAM266@users.noreply.github.com>
2022-08-30 18:16:58 +0530

    Time: 0 ms (100.00%), Space: 6 MB (27.08%) - LeetHub

--- 231-power-of-two/231-power-of-two.cpp
+++ 231-power-of-two/231-power-of-two.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
+        ios_base::sync_with_stdio(0);
+    	// cin.tie(nullptr);
+    	cout.tie(nullptr);
         if (n <= 0) return false;
         if (!(n & (n-1))) return true;
         return false;
