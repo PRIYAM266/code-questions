87e96e77824e46d75062de3fbef7b28383791ddc
Priyam <54617669+PRIYAM266@users.noreply.github.com>
2022-08-31 01:20:16 +0530

    Time: 44 ms (19.51%), Space: 17.8 MB (93.55%) - LeetHub

---
+++ 268-missing-number/268-missing-number.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int numsXor = 0;
+        for (int i = 0; i < n; i++) {
+            numsXor ^= nums[i];
+        }
+        int naturalXor = 0;
+        
+        if (n % 4 == 0)
+            naturalXor = n;
+        else if (n % 4 == 1)
+            naturalXor = 1;
+        else if (n % 4 == 2)
+            naturalXor = n+1;
+        else 
+            naturalXor = 0;
+        
+        return naturalXor ^ numsXor;
+    }
+};
