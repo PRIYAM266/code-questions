58601526165373a9eaa6b9f6508e349e1698522c
Priyam <54617669+PRIYAM266@users.noreply.github.com>
2022-09-03 19:10:53 +0530

    Time: 9 ms (5.16%), Space: 8.4 MB (64.48%) - LeetHub

--- 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.cpp
@@ -12,16 +12,56 @@
 class Solution {
 public:
     
-    void postorder(TreeNode* root, vector<int>& ans) {
-        if (root == NULL) return;
-        postorder(root->left, ans);
-        postorder(root->right, ans);
-        ans.push_back(root->val);
+    // void postorder(TreeNode* root, vector<int>& ans) {
+    //     if (root == NULL) return;
+    //     postorder(root->left, ans);
+    //     postorder(root->right, ans);
+    //     ans.push_back(root->val);
+    // }
+    
+    vector<int> preInPost(TreeNode* root) {
+        vector<int> pre, in, post;
+        if (root == NULL)
+            return pre;
+        
+        stack<pair<TreeNode*, int>> st;
+        st.push({root, 1});
+        
+        while(!st.empty()) {
+            auto it = st.top();
+            st.pop();
+            
+            if (it.second == 1) {
+                pre.push_back(it.first->val);
+                it.second++;
+                st.push(it);
+                
+                if (it.first->left) {
+                    st.push({it.first->left, 1});
+                }
+            }
+            
+            else if (it.second == 2) {
+                in.push_back(it.first->val);
+                it.second++;
+                st.push(it);
+                
+                if(it.first->right) {
+                    st.push({it.first->right, 1});
+                }
+            }
+            else {
+                post.push_back(it.first->val);
+            }
+        }
+        
+        return post;
     }
     
     vector<int> postorderTraversal(TreeNode* root) {
-        vector<int> ans;
-        postorder(root, ans);
-        return ans;
+        // vector<int> ans;
+        // postorder(root, ans);
+        // return ans;
+        return preInPost(root);
     }
 };
