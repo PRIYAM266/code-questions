1952bbbcf28c5d6c46bf5b080f57a738bc07d92f
Priyam <54617669+PRIYAM266@users.noreply.github.com>
2022-09-03 20:06:23 +0530

    Time: 16 ms (42.54%), Space: 18.8 MB (55.33%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.cpp
@@ -11,27 +11,38 @@
  */
 class Solution {
 public:
+    
+    int f(TreeNode* root) {
+        if (root == NULL)
+            return 0;
+        int lh = f(root->left);
+        int rh = f(root->right);
+        
+        return 1 + max(lh, rh);
+    }
+    
     int maxDepth(TreeNode* root) {
         ios_base::sync_with_stdio(0);
     	cin.tie(nullptr);
     	
-        if (root == NULL) return 0;
-        int depth = 0;
-        queue<TreeNode*> q;
-        q.push(root);
-        while(!q.empty()) {
-            int n = q.size();
-            depth++;
-            for (int i = 0; i < n; i++) {
-                TreeNode* tmp = q.front();
-                q.pop();
+//         if (root == NULL) return 0;
+//         int depth = 0;
+//         queue<TreeNode*> q;
+//         q.push(root);
+//         while(!q.empty()) {
+//             int n = q.size();
+//             depth++;
+//             for (int i = 0; i < n; i++) {
+//                 TreeNode* tmp = q.front();
+//                 q.pop();
                 
-                if (tmp->left)
-                    q.push(tmp->left);
-                if (tmp->right)
-                    q.push(tmp->right);
-            }
-        }
-        return depth;
+//                 if (tmp->left)
+//                     q.push(tmp->left);
+//                 if (tmp->right)
+//                     q.push(tmp->right);
+//             }
+//         }
+//         return depth;
+        return f(root);
     }
 };
