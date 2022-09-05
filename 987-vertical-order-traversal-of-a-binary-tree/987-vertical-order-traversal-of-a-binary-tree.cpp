556c404d40750969826c566edf8dfe8a210bb63e
Priyam <54617669+PRIYAM266@users.noreply.github.com>
2022-09-05 19:50:45 +0530

    Time: 14 ms (20.01%), Space: 13.4 MB (63.52%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -15,25 +15,25 @@ public:
         ios_base::sync_with_stdio(0);
     	cin.tie(nullptr);
 
-        map<int, map<int, multiset<int>>> nodes;
-        queue<pair<TreeNode*, pair<int, int>>> todo;
-        todo.push({root, {0, 0}});
-        while(!todo.empty()) {
-            auto p = todo.front();
-            todo.pop();
+        map<int, map<int, multiset<int>>> mp;
+        queue<pair<TreeNode*, pair<int, int>>> q;
+        q.push({root, {0, 0}});
+        while(!q.empty()) {
+            auto p = q.front();
+            q.pop();
             TreeNode* node = p.first;
             int x = p.second.first, y = p.second.second;
-            nodes[x][y].insert(node->val);
+            mp[x][y].insert(node->val);
             if (node->left)
-                todo.push({node->left, {x-1, y+1}});
+                q.push({node->left, {x-1, y+1}});
             if (node->right)
-                todo.push({node->right, {x+1, y+1}});
+                q.push({node->right, {x+1, y+1}});
         }
         // for (auto i: nodes) {
         //     cout << i.first << " " << i.second.first << " " << i.second.second << endl;
         // }
         vector<vector<int>> ans;
-        for (auto p : nodes) {
+        for (auto p : mp) {
             vector<int> col;
             for (auto q : p.second) {
                 col.insert(col.end(), q.second.begin(), q.second.end());
