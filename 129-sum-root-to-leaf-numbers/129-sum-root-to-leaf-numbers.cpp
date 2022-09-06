02038cada2258698a15c47bf318f5c6f2dce22a9
Priyam <54617669+PRIYAM266@users.noreply.github.com>
2022-09-07 00:43:38 +0530

    Time: 4 ms (48.99%), Space: 9.4 MB (19.29%) - LeetHub

--- 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.cpp
+++ 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.cpp
@@ -11,27 +11,31 @@
  */
 class Solution {
 public:
-    vector<string> v;
-    void path(TreeNode* node, string curr) {
+    void path(TreeNode* node, int tmp, int& ans) {
+        if (node == NULL)
+            return;
+        
+        tmp = tmp * 10 + node->val;
         if (node->left == NULL && node->right == NULL) {
-            curr += to_string(node->val);
-            v.push_back(curr);
+            ans += tmp;
         }
-        curr += to_string(node->val);
+        
         if (node->left)
-            path(node->left, curr);
+            path(node->left, tmp, ans);
         if (node->right)
-            path(node->right, curr);
+            path(node->right, tmp, ans);
     }
     
     int sumNumbers(TreeNode* root) {
-        // vector<string> v;
-        string curr = "";
-        path(root, curr);
+        ios_base::sync_with_stdio(0);
+    	cin.tie(nullptr);
+    	
         int ans = 0;
-        for (int i = 0; i < v.size(); i++) {
-            ans += stoi(v[i]);
-        }
+        path(root, 0, ans);
+        
+        // for (int i = 0; i < v.size(); i++) {
+        //     ans += stoi(v[i]);
+        // }
         return ans;
     }
 };
