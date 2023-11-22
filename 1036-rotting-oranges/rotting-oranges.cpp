93c299c8fb016a2bb3ad0f80cb65cf51280664e5
Priyam <54617669+PRIYAM266@users.noreply.github.com>
2023-11-22 16:33:41 +0530

    Time: 7 ms (46.04%) | Memory: 13.8 MB (25.65%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,5 +1,28 @@
 class Solution {
 public:
+    void bfs(queue< pair< pair<int, int>, int>>& q, int& time, vector<vector<int>>& grid,
+        vector<vector<int>>& vis) {
+            int delRow[] = {-1, 0, 1, 0};
+            int delCol[] = {0, -1, 0, 1};
+            int m = grid.size(), n = grid[0].size();
+
+            while(!q.empty()) {
+                int row = q.front().first.first;
+                int col = q.front().first.second;
+                int t = q.front().second;
+                time = max(time, t);
+                q.pop();
+                for (int i = 0; i < 4; i++) {
+                    int nrow = row + delRow[i];
+                    int ncol = col + delCol[i];
+                    if (nrow >= 0 && nrow < m && ncol >= 0 && ncol < n && 
+                        vis[nrow][ncol] == 1 && grid[nrow][ncol] == 1) {
+                            q.push({{nrow, ncol}, t + 1});
+                            vis[nrow][ncol] = 2;
+                        }
+                }
+            }
+        }
     int orangesRotting(vector<vector<int>>& grid) {
         int m = grid.size(), n = grid[0].size();
         vector<vector<int>> vis(m, vector<int>(n));
@@ -14,24 +37,8 @@ public:
             }
         }
         int tm = 0;
-        int delRow[] = {-1, 0, 1, 0};
-        int delCol[] = {0, -1, 0, 1};
-        while(!q.empty()) {
-            int row = q.front().first.first;
-            int col = q.front().first.second;
-            int t = q.front().second;
-            tm = max(tm, t);
-            q.pop();
-            for (int i = 0; i < 4; i++) {
-                int nrow = row + delRow[i];
-                int ncol = col + delCol[i];
-                if (nrow >= 0 && nrow < m && ncol >= 0 && ncol < n && vis[nrow][ncol] == 1 
-                    && grid[nrow][ncol] == 1) {
-                        q.push({{nrow, ncol}, t + 1});
-                        vis[nrow][ncol] = 2;
-                    }
-            }
-        }
+        
+        bfs(q, tm, grid, vis);
         
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
